10e07650faf7566e066650379a216a56ebad90c9
Niemaly <jac3k2@gmail.com>
2020-01-27 18:40:39 +0100

    Green step.

--- src/main/java/src/googleSumTask/GoogleTaskImpl.java
+++ src/main/java/src/googleSumTask/GoogleTaskImpl.java
@@ -1,7 +1,18 @@
 package src.googleSumTask;
 
+import java.util.Arrays;
+
 public class GoogleTaskImpl {
     public String isThereOrderedSum(int[] input, int orderedSum) {
-        return null;
+        //Brutal Force
+
+
+        for (int i = 0; i < input.length-1; i++) {
+            for (int j = i+1; j <input.length ; j++) {
+                if ((input[i]+input[j])==orderedSum)
+                    return "YES";
+            }
+        }
+        return "NO";
     }
 }
