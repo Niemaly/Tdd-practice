92541b4f5d96248554d3890b5443040efc2df3ae
Niemaly <jac3k2@gmail.com>
2020-01-30 13:10:31 +0100

    final version.

--- src/main/java/src/codewars/isItVowel/StringUtils.java
+++ src/main/java/src/codewars/isItVowel/StringUtils.java
@@ -2,10 +2,10 @@ package src.codewars.isItVowel;
 
 public class StringUtils {
 
-    private static final String VOWELS = "AEIOU";
+    private static final String VOWELS = "[AEIOUaeiou]";
 
     public static Boolean isVowel(String s) {
-        return s.matches("[" + VOWELS+ "]") || s.matches("[" + VOWELS.toLowerCase() + "]");
+        return s.matches(VOWELS);
     }
 
 }
