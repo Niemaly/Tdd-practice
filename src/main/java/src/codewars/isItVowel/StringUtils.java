7951eb09efd8461797eaeac6015c67a0e9ecd12b
Niemaly <jac3k2@gmail.com>
2020-01-30 13:09:35 +0100

    refactoring

--- src/main/java/src/codewars/isItVowel/StringUtils.java
+++ src/main/java/src/codewars/isItVowel/StringUtils.java
@@ -4,18 +4,8 @@ public class StringUtils {
 
     private static final String VOWELS = "AEIOU";
 
-
     public static Boolean isVowel(String s) {
-
-        if (s.length() > 1)
-            return false;
-
-        for (char vowel : VOWELS.toCharArray()) {
-            if (s.toUpperCase().contains(String.valueOf(vowel))) {
-                return true;
-            }
-        }
-
-        return false;
+        return s.matches("[" + VOWELS+ "]") || s.matches("[" + VOWELS.toLowerCase() + "]");
     }
+
 }
